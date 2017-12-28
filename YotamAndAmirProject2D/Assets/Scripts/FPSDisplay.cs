d5973853a6ecddec66a6958a6b6e3a001eec74ab
amir weinfeld <amirweinfeld@gmail.com>
2017-12-28 12:01:58 +0200

    Canvas Improves

--- YotamAndAmirProject2D/Assets/Scripts/FPSDisplay.cs
+++ YotamAndAmirProject2D/Assets/Scripts/FPSDisplay.cs
@@ -19,7 +19,7 @@ public class FPSDisplay : MonoBehaviour
         GUIStyle style = new GUIStyle();
 
         Rect rect = new Rect(0, 0, w, h * 2 / 100);
-        style.alignment = TextAnchor.UpperLeft;
+        style.alignment = TextAnchor.UpperRight;
         style.fontSize = h * 2 / 100;
         style.normal.textColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);
         float msec = deltaTime * 1000.0f;
